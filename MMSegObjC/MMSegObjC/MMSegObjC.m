b6b162dbc946fe6d5b73cb6e6b83109ea7da69fb
Andrew(Zhiyong) Yang <AndrewYang@ZhiyongMBP.local>
2015-12-30 20:11:38 +0800

    Setup project.

---
+++ MMSegObjC/MMSegObjC/MMSegObjC.m
@@ -0,0 +1,13 @@
+//
+//  MMSegObjC.m
+//  MMSegObjC
+//
+//  Created by Andrew(Zhiyong) Yang on 12/30/15.
+//  Copyright © 2015 FoolDragon. All rights reserved.
+//
+
+#import "MMSegObjC.h"
+
+@implementation MMSegObjC
+
+@end
